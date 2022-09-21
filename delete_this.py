ac54ba17dd3b9c0e122abd7e50d484d0af8d7a34
csknyy <csknyy@hotmail.com>
2022-09-21 17:17:26 +1200

    update

---
+++ delete_this.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.set_page_config(page_title="Amazon orders", layout="wide")
+
+data = pd.DataFrame(columns=['A'], index=[0])
+
+file = st.file_uploader("Drag and drop a file")
+
+st.dataframe(data)
