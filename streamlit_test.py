b7a0d79c524cae556c42ce1bdeb41b7058712d63
csknyy <csknyy@hotmail.com>
2022-09-22 13:17:55 +1200

    update

--- streamlit_test.py
+++ streamlit_test.py
@@ -5,12 +5,15 @@ st.set_page_config(page_title="Amazon orders", layout="wide")
 
 data = pd.DataFrame
 
+url = "https://s84.etcserver.com/~coskunyay/worldfront/all_orders.csv"
+#url = "https://www.coskunyay.com/worldfront/all_orders.csv"
+
 file = st.file_uploader("Drag and drop a file")
 
 try:
     data = pd.read_csv(file)
 except:
-    data = pd.read_csv('all_orders.csv')
+    data = pd.read_csv(url)
 
 #COLUMNS
 cols = data.columns.to_list()
@@ -23,6 +26,7 @@ data.columns = cols
 #SIDEBAR
 
 st.sidebar.header("Filters")
+group_by = st.sidebar.multiselect("Group by",options = ['Barcode','Category','Country','Channel','Supplier','Priced_At_supplier','Order_Status'], default = ['Channel'])
 status = st.sidebar.multiselect("Order status",options = data["Order_Status"].unique(), default = data["Order_Status"].unique())
 columns = st.sidebar.multiselect("Columns",options = cols, default = cols)
 channel = st.sidebar.multiselect("Channel",options = data["Channel"].unique(), default = data["Channel"].unique())
@@ -32,9 +36,10 @@ pri_supplier = st.sidebar.multiselect("Priced at supplier",options = data["Price
 data_selection = data.query("Order_Status == @status & Channel == @channel & Supplier == @supplier & Priced_At_supplier == @pri_supplier")
 data_selection = data_selection[columns]
 
-
-
-data_groupby = data_selection.groupby(by=["Channel","Order_Status"]).sum()[['Qty','Total_USD']]
+if len(group_by) == 0:
+    data_groupby = pd.DataFrame
+else:
+    data_groupby = data_selection.groupby(by=group_by).sum()[['Qty','Total_USD']].sort_values(by='Qty', ascending= False)
 
 total_revenue = data_selection["Total_USD"].sum()
 total_revenue = int(total_revenue*100)/100
@@ -42,21 +47,24 @@ total_qty = data_selection["Qty"].sum()
 average = total_revenue/total_qty
 average = int(average*100)/100
 
+
+st.markdown("---")
+
+st.subheader(f"Grouped by {group_by}")
+
+st.dataframe(data_groupby)
+
 left_column, middle_column, right_column = st.columns(3)
 with left_column:
-    st.subheader("Total revenue")
-    st.subheader(f"{total_revenue:,}")
-with middle_column:
     st.subheader("Total quantity")
     st.subheader(f"{total_qty:,}")
+with middle_column:
+    st.subheader("Total revenue")
+    st.subheader(f"{total_revenue:,} USD")
 with right_column:
     st.subheader("Average")
     st.subheader(f"{average:,}")
 
 st.markdown("---")
 
-st.dataframe(data_groupby)
-
 st.dataframe(data_selection)
-
-print("Upload a file")
