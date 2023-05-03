714852cd31487120ac3afe71dfae675e8a36543d
csknyy <csknyy@hotmail.com>
2023-05-03 16:09:24 +1200

    Update missed_promise_date.py

--- missed_promise_date.py
+++ missed_promise_date.py
@@ -242,7 +242,7 @@ data_boxscore0['Promise_Shipped'] = data_boxscore0['Promise_Date'] - data_boxsco
 data_boxscore0['Promise_Shipped'] = [int(100 * i.total_seconds() / (24 * 60 * 60)) / 100 for i in
                                      data_boxscore0['Promise_Shipped']]
 
-data_boxscore0_2 = data_boxscore0.iloc[:,3:].groupby(by='Priced_at_supplier').sum()
+data_boxscore0_2 = data_boxscore0.groupby(by='Priced_at_supplier').sum()
 
 st.dataframe(data_boxscore0_2)
 
