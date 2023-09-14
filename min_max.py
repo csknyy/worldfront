6446ac9d86ef754771582451b0a604a53388cd82
csknyy <55134627+csknyy@users.noreply.github.com>
2023-09-15 08:25:19 +1200

    Update min_max.py

--- min_max.py
+++ min_max.py
@@ -8,63 +8,63 @@ def convert_df(df):
 
 uploaded_file = st.file_uploader("Choose the .csv file")
 
-dataframe = pd.read_csv(uploaded_file, header=1)
+df = pd.read_csv(uploaded_file, header=1)
 
 list1 = []
 
-for i in range(len(dataframe)):
-  if dataframe['Average Weekly Sales'][i] == 0:
+for i in range(len(df)):
+  if df['Average Weekly Sales'][i] == 0:
     list1.append(0)
-  elif dataframe['Department'][i] == "300 AUTO AND TOOL STORAGE":
-    if (dataframe['Average Weekly Sales'][i] * 5)//1 + 1 == (dataframe['Average Weekly Sales'][i] * 5)/1 + 1:
-      list1.append((dataframe['Average Weekly Sales'][i] * 5)//1)
+  elif df['Department'][i] == "300 AUTO AND TOOL STORAGE":
+    if (df['Average Weekly Sales'][i] * 5)//1 + 1 == (df['Average Weekly Sales'][i] * 5)/1 + 1:
+      list1.append((df['Average Weekly Sales'][i] * 5)//1)
     else:
-      list1.append((dataframe['Average Weekly Sales'][i] * 5)//1 + 1)
+      list1.append((df['Average Weekly Sales'][i] * 5)//1 + 1)
   else:
-    if (dataframe['Average Weekly Sales'][i] * 4)//1 + 1 == (dataframe['Average Weekly Sales'][i] * 4)/1 + 1:
-      list1.append((dataframe['Average Weekly Sales'][i] * 4)//1)
+    if (df['Average Weekly Sales'][i] * 4)//1 + 1 == (df['Average Weekly Sales'][i] * 4)/1 + 1:
+      list1.append((df['Average Weekly Sales'][i] * 4)//1)
     else:
-      list1.append((dataframe['Average Weekly Sales'][i] * 4)//1 + 1)
+      list1.append((df['Average Weekly Sales'][i] * 4)//1 + 1)
 
-dataframe["Min for Upload Supplier Request1"] = list1
+df["Min for Upload Supplier Request1"] = list1
 
 list1 = []
 
-for i in range(len(dataframe)):
-  if dataframe['Average Weekly Sales'][i] == 0:
+for i in range(len(df)):
+  if df['Average Weekly Sales'][i] == 0:
     list1.append(0)
-  elif dataframe['Department'][i] == "300 AUTO AND TOOL STORAGE":
-    if (dataframe['Average Weekly Sales'][i] * 8)//1 + 1 == (dataframe['Average Weekly Sales'][i] * 8)/1 + 1:
-      list1.append((dataframe['Average Weekly Sales'][i] * 8)//1)
+  elif df['Department'][i] == "300 AUTO AND TOOL STORAGE":
+    if (df['Average Weekly Sales'][i] * 8)//1 + 1 == (df['Average Weekly Sales'][i] * 8)/1 + 1:
+      list1.append((df['Average Weekly Sales'][i] * 8)//1)
     else:
-      list1.append((dataframe['Average Weekly Sales'][i] * 8)//1 + 1)
-  elif dataframe['Department'][i] == "300 PAINT":
-    if (dataframe['Average Weekly Sales'][i] * 5)//1 + 1 == (dataframe['Average Weekly Sales'][i] * 5)/1 + 1:
-      list1.append((dataframe['Average Weekly Sales'][i] * 5)//1)
+      list1.append((df['Average Weekly Sales'][i] * 8)//1 + 1)
+  elif df['Department'][i] == "300 PAINT":
+    if (df['Average Weekly Sales'][i] * 5)//1 + 1 == (df['Average Weekly Sales'][i] * 5)/1 + 1:
+      list1.append((df['Average Weekly Sales'][i] * 5)//1)
     else:
-      list1.append((dataframe['Average Weekly Sales'][i] * 5)//1 + 1)
+      list1.append((df['Average Weekly Sales'][i] * 5)//1 + 1)
   else:
-    if (dataframe['Average Weekly Sales'][i] * 6)//1 + 1 == (dataframe['Average Weekly Sales'][i] * 6)/1 + 1:
-      list1.append((dataframe['Average Weekly Sales'][i] * 6)//1)
+    if (df['Average Weekly Sales'][i] * 6)//1 + 1 == (df['Average Weekly Sales'][i] * 6)/1 + 1:
+      list1.append((df['Average Weekly Sales'][i] * 6)//1)
     else:
-      list1.append((dataframe['Average Weekly Sales'][i] * 6)//1 + 1)
+      list1.append((df['Average Weekly Sales'][i] * 6)//1 + 1)
 
-dataframe["Max for Upload Supplier Request1"] = list1
+df["Max for Upload Supplier Request1"] = list1
 
 list1 = []
-for i in range(len(dataframe)):
-  list1.append(max(dataframe["Current Min"][i],dataframe["Min for Upload Supplier Request1"][i]))
+for i in range(len(df)):
+  list1.append(max(df["Current Min"][i],df["Min for Upload Supplier Request1"][i]))
 
 dataframe["Min for Upload Supplier Request"] = list1
 
 list1 = []
-for i in range(len(dataframe)):
-  list1.append(max(dataframe["Current Max"][i],dataframe["Max for Upload Supplier Request1"][i]))
+for i in range(len(df)):
+  list1.append(max(df["Current Max"][i],df["Max for Upload Supplier Request1"][i]))
 
-dataframe["Max for Upload Supplier Request"] = list1
+df["Max for Upload Supplier Request"] = list1
 
-st.dataframe(dataframe)
+st.dataframe(df)
 
-csv = convert_df(dataframe)
+csv = convert_df(df)
 
 st.download_button(label="Download data as CSV", data=csv,file_name='Concatenated_files.csv', mime='text/csv')
