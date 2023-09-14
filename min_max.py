1dfca385c594113d907a31e42a82eb92e206dd6e
csknyy <55134627+csknyy@users.noreply.github.com>
2023-09-15 08:15:32 +1200

    Update min_max.py

--- min_max.py
+++ min_max.py
@@ -6,24 +6,62 @@ st.set_page_config(page_title="Min Max", layout="wide")
 def convert_df(df):
     return df.to_csv().encode('utf-8')
 
-#file_count = st.text_input('How many files?', '')
+uploaded_file = st.file_uploader("Choose the Excel file")
 
-#files = [f"file{i}" for i in range(int(file_count))]
+dataframe = pd.read_excel(uploaded_file)
 
-uploaded_files = st.file_uploader("Choose the CSV files", accept_multiple_files=True)
-file_count = len(uploaded_files)
+list1 = []
 
-files = [f"file{i}" for i in range(int(file_count))]
+for i in range(len(dataframe)):
+  if dataframe['Average Weekly Sales'][i] == 0:
+    list1.append(0)
+  elif dataframe['Department'][i] == "300 AUTO AND TOOL STORAGE":
+    if (dataframe['Average Weekly Sales'][i] * 5)//1 + 1 == (dataframe['Average Weekly Sales'][i] * 5)/1 + 1:
+      list1.append((dataframe['Average Weekly Sales'][i] * 5)//1)
+    else:
+      list1.append((dataframe['Average Weekly Sales'][i] * 5)//1 + 1)
+  else:
+    if (dataframe['Average Weekly Sales'][i] * 4)//1 + 1 == (dataframe['Average Weekly Sales'][i] * 4)/1 + 1:
+      list1.append((dataframe['Average Weekly Sales'][i] * 4)//1)
+    else:
+      list1.append((dataframe['Average Weekly Sales'][i] * 4)//1 + 1)
 
-i=0
-for uploaded_file,file in zip(uploaded_files,files):
-    file = pd.read_csv(uploaded_file)
-    files[i] = file
-    st.write("Uploaded:", uploaded_file.name)
-    i = i+1
+dataframe["Min for Upload Supplier Request1"] = list1
 
-st.header("Concatenated dataframes")
-result = pd.concat(files)
+list1 = []
+
+for i in range(len(dataframe)):
+  if dataframe['Average Weekly Sales'][i] == 0:
+    list1.append(0)
+  elif dataframe['Department'][i] == "300 AUTO AND TOOL STORAGE":
+    if (dataframe['Average Weekly Sales'][i] * 8)//1 + 1 == (dataframe['Average Weekly Sales'][i] * 8)/1 + 1:
+      list1.append((dataframe['Average Weekly Sales'][i] * 8)//1)
+    else:
+      list1.append((dataframe['Average Weekly Sales'][i] * 8)//1 + 1)
+  elif dataframe['Department'][i] == "300 PAINT":
+    if (dataframe['Average Weekly Sales'][i] * 5)//1 + 1 == (dataframe['Average Weekly Sales'][i] * 5)/1 + 1:
+      list1.append((dataframe['Average Weekly Sales'][i] * 5)//1)
+    else:
+      list1.append((dataframe['Average Weekly Sales'][i] * 5)//1 + 1)
+  else:
+    if (dataframe['Average Weekly Sales'][i] * 6)//1 + 1 == (dataframe['Average Weekly Sales'][i] * 6)/1 + 1:
+      list1.append((dataframe['Average Weekly Sales'][i] * 6)//1)
+    else:
+      list1.append((dataframe['Average Weekly Sales'][i] * 6)//1 + 1)
+
+dataframe["Max for Upload Supplier Request1"] = list1
+
+list1 = []
+for i in range(len(dataframe)):
+  list1.append(max(dataframe["Current Min"][i],dataframe["Min for Upload Supplier Request1"][i]))
+
+dataframe["Min for Upload Supplier Request"] = list1
+
+list1 = []
+for i in range(len(dataframe)):
+  list1.append(max(dataframe["Current Max"][i],dataframe["Max for Upload Supplier Request1"][i]))
+
+dataframe["Max for Upload Supplier Request"] = list1
 
 st.dataframe(result)
 
