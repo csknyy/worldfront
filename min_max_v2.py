599f1261b8cd5fbe95d83c1ed5ed3a8021821881
csknyy <55134627+csknyy@users.noreply.github.com>
2023-09-15 11:00:37 +1200

    Update min_max_v2.py

--- min_max_v2.py
+++ min_max_v2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 
-st.set_page_config(page_title="Min Max", layout="wide")
+st.set_page_config(page_title="Min Max v2.0", layout="wide")
 
 def convert_data(data):
     return data.to_csv(index=False).encode('utf-8')
