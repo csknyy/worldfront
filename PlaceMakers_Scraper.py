25fd79655f378b6fa11fd24d55de2e8426470e2c
csknyy <55134627+csknyy@users.noreply.github.com>
2023-09-18 16:35:12 +1200

    Update PlaceMakers_Scraper.py

--- PlaceMakers_Scraper.py
+++ PlaceMakers_Scraper.py
@@ -112,8 +112,58 @@ def on_button_click():
 
     st.dataframe(data)
 
+##########
+#The Warehouse
+##########
+
+def on_button_click():
+    st.write("Scrapping started")
+
+    url = "https://www.thewarehouse.co.nz/search?prefn1=brand&prefv1=Ados%7CCRC&sz=64"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+
+    products = response.text.split('a href="/p/')
+
+    link_list = []
+    id_list = []
+    name_list = []
+    price_list = []
+    crc_code_list = []
+    
+    for i in range(1,len(products)):
+      link_list.append("https://www.thewarehouse.co.nz/p/" + products[i].split('"')[0])
+      id_list.append(link_list[i-1].split("/")[-1].split(".")[0])
+    
+    items_done = 1
+    
+    for item_url in link_list:
+      response = requests.get(item_url)
+      name_list.append(response.text.split('html","name":"')[1].split('"')[0])
+      try:
+        crc_code_list.append(response.text.split('},"mpn":"')[1].split('"')[0])
+      except:
+        crc_code_list.append("")
+      price_list.append(response.text.split('","price":"')[1].split('"')[0])
+      st.write(f"{items_done} / {len(link_list)} done")
+      items_done += 1
+    
+    data = pd.DataFrame()
+    data['Name'] = name_list
+    data['CRC Code'] = crc_code_list
+    data['The Warehouse SKU'] = id_list
+    data['Price'] = price_list
+    data['Link'] = link_list
+
+    st.dataframe(data)
+
+###################################################################
+
 if st.button("Scrape PlaceMakers"):
     on_button_click()
 
 if st.button("Scrape Super Cheap Auto"):
+    on_button_click()
+
+if st.button("Scrape The Warehouse"):
     on_button_click()
