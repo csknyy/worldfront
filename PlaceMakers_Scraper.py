7dd50b2e0450f693656d0fe4dac3b1ab4866c27c
csknyy <55134627+csknyy@users.noreply.github.com>
2024-03-26 13:28:37 +1300

    Update PlaceMakers_Scraper.py

--- PlaceMakers_Scraper.py
+++ PlaceMakers_Scraper.py
@@ -15,7 +15,7 @@ def clean_string(text):
 
 
 ##########
-#PlaceMakers
+# NZ - PlaceMakers
 ##########
 
 def on_button_click_Place():
@@ -84,7 +84,7 @@ def on_button_click_Place():
     st.dataframe(merged_data)
 
 ##########
-#Super Cheap Auto
+# NZ - Super Cheap Auto
 ##########
 
 def on_button_click_Super():
@@ -170,7 +170,13 @@ def on_button_click_Super():
         'CRC Dry Glide - 150mL' : '3040',
         'Lanocote Multi-Purpose Lubricant - 500mL' : '3020',
         'CRC Clean-R-Carb - 400g' : '5081',
-        'CRC Battery Maintenance Cleaner 300g' : '5097'
+        'CRC Battery Maintenance Cleaner 300g' : '5097',
+        'CRC Prep It - 400mL' : '2114',
+        'CRC AC Charge Refrigerant R134a Air Conditioner Refill - 400g' : '5101',
+        'CRC AC Charge Refrigerant R134a Refill  and  Hose - 400g' : '5100',
+        'CRC Etch It - 400mL' : '2110',
+        'CRC Prime It - 400mL' : '2091',
+        'CRC Battery Terminal Protector 300g' : '5098'
     }
     
     crc_code_list = []
@@ -196,7 +202,7 @@ def on_button_click_Super():
     st.dataframe(merged_data)
 
 ##########
-#The Warehouse
+# NZ - The Warehouse
 ##########
 
 def on_button_click_Warehouse():
@@ -246,7 +252,7 @@ def on_button_click_Warehouse():
     st.dataframe(merged_data)
 
 ##########
-#The ToolShed
+# NZ - The ToolShed
 ##########
 
 def on_button_click_ToolShed():
@@ -353,10 +359,10 @@ def on_button_click_ToolShed():
     st.dataframe(merged_data)
 
 ##########
-#AU - Total Tools
+# AU - Total Tools
 ##########
 
-def on_button_click_TotalTools():
+def on_button_click_AU_TotalTools():
     st.write("Scrapping started")
 
     url = "https://www.totaltools.com.au/brands/crc?p=1&product_list_limit=48"
@@ -408,7 +414,137 @@ def on_button_click_TotalTools():
     data['Total Tools SKU'] = id_list
     data['Price'] = price_list
 
-    st.dataframe(data)   
+    st.dataframe(data)
+
+##########
+# NZ - Super Cheap Auto
+##########
+
+def on_button_click_NZ_Super():
+    st.write("Scrapping started")
+    
+    url = "https://www.supercheapauto.com.au/search?prefn1=srgBrand&prefv1=CRC%7CADOS&sz=60"
+    response = requests.get(url)
+    
+    products = response.text.split(';" title="')
+    
+    name_list, id_list, price_list, link_list, first_price_list = [],[],[],[],[]
+    
+    for i in range(1, len(products)):
+      if "<" in products[i].split('">')[0]:
+        pass
+      else:
+        name_list.append(products[i].split('">')[0])
+      if "<" in products[i].split("/images/")[1].split("/")[0]:
+        pass
+      else:
+        id_list.append(products[i].split("/images/")[1].split("/")[0])
+      try:
+        price_list.append(products[i].split('price">$')[1].split('<')[0])
+      except:
+        price_list.append("")
+      try:
+        first_price_list.append(products[i].split('price">$')[2].split('<')[0])
+      except:
+        first_price_list.append("")
+      try:
+        link_list.append("https://www.supercheapauto.com.au" + products[i].split('href="')[1].split('"')[0])
+      except:
+        link_list.append("")
+    
+    data = pd.DataFrame()
+    data['Name'] = name_list
+    
+    CRC_name_code_dict = {
+        'CRC Aeroclean Degreaser - 400g' : '5070',
+        'CRC White Lithium Grease - 300g' : '5037',
+        'CRC CO Contact Cleaner - 500mL' : '2016',
+        'CRC Black It Enamel Paint, Gloss Black - 400g' : '5111',
+        'CRC Surface Sanitiser Spray - 500mL' : '1752080',
+        'CRC 5.56 Permastraw Multi-Purpose Lubricant 380mL' : '5029',
+        'Multi-Lube Gel' : '5018',
+        '808 Silicone Spray - 500mL' : '3055',
+        'CRC Soft Seal Spray Protectant 400g' : '3013',
+        'CRC Brakleen Blaster - 600g' : '5087',
+        '5.56 Multi-Purpose Lubricant 420mL' : '1751837',
+        'CRC Auto AC Pro Air Conditioner Cleaner' : '1753204',
+        'CRC Black Zinc - 300g' : '2089',
+        'CRC Engine Start 400mL' : '5040',
+        'CRC Maniseal Exhaust Cement - Brown, 145g' : '5061',
+        'Fuel Stabiliser 350mL' : '2815',
+        'CRC Brakleen Brake and Parts Cleaner 600g' : '5089',
+        'ADOS Contact Adhesive - F2 Multipurpose, 75ml' : '8002',
+        'CRC Rust Converter 1 Litre' : '18418',
+        'CRC Zinc It - 350g' : '2085',
+        'CRC Rust Converter Aerosol 425g' : '14610',
+        'CRC Tackle Guard Reel Lube 130ml' : '6028',
+        'CRC De-Icer Aerosol - 400mL' : '5044',
+        'CRC Rust Converter 250mL' : '3073',
+        'Penetr8 - 210ml' : '5500',
+        'CRC Noxy Spray Lubricant 400g' : '3027',
+        'CRC Black It Enamel Paint, Matt Black - 400g' : '5110',
+        'CRC Dust Buster - 250mL' : '2071',
+        'ADOS Spray Adhesive - Multipurpose, 210ml' : '8015',
+        'CRC Bright Zinc - 400mL' : '2087',
+        'Evapo-Rust Spray Gel 500g' : '1753336',
+        'CRC Salt Terminator Mixer - 946mL' : 'SX32M',
+        'CRC Fiberlock Head Gasket Repair - 946mL' : '1224',
+        'CRC Mass Air Flow Sensor Cleaner 400mL' : '5093',
+        'ADOS Spray Adhesive - Multipurpose, 575ml' : '8017',
+        'ADOS Contact Adhesive - F2 Multipurpose, 500ml' : '8009',
+        'CRC Welding Anti-Spatter Spray - 300g' : '3358',
+        'ADOS Contact Adhesive - F2 Multipurpose, 250ml' : '8008',
+        'CRC Oil Fighter - 400mL' : '5069',
+        'CRC GDI Intake Valve Cleaner - 400mL' : '5095',
+        'Marine 556 Lubricant 420mL' : '1751839',
+        'CRC Throttle Body Cleaner 500mL' : '5077',
+        'ADOS Adhesive Remover Stuff Off - 500ml' : '8270',
+        'CRC Belt Grip 400ml' : '3081',
+        'ADOS Spray Adhesive - High Strength, 550ml' : '8180',
+        'CRC Black It Enamel Paint, Satin Black - 400g' : '5112',
+        'CRC Leak Stop Spray Seal 350g' : '8498',
+        'CRC 5.56 Multi Purpose Lubricant - 4 Litre' : '1751846',
+        'CRC Dry Glide - 150mL' : '3040',
+        'Lanocote Multi-Purpose Lubricant - 500mL' : '3020',
+        'CRC Clean-R-Carb - 400g' : '5081',
+        'CRC Battery Maintenance Cleaner 300g' : '5097',
+        'CRC Mass Air Flow Sensor Cleaner 300g' : '5014',
+        'CRC Throttle Body and Air Intake Cleaner 400g' : '5079',
+        'CRC Battery Terminal Protector 300g' : '5098',
+        '5.56 Multi-Purpose Lubricant CRC 400g' : '5005',
+        'CRC Oil Fighter Oil Stain Remover - 400mL' : '1751967',
+        'CRC Automotive Electronic Cleaner - 350g' : '5013',
+        'CRC Aerostart 300g' : '5051',
+        'Lanoshield CRC 350g' : '3051',
+        'Marine 66 CRC 300g' : '6006',
+        'CRC Prep It - 400mL' : '2114',
+        'CRC Prime It - 400mL' : '2091',
+        'CRC Etch It - 400mL' : '2110',
+        'CRC SmartWasher Mobile Parts Cleaning Startup Kit - SW-23-1' : 'SW-23'
+    }
+    
+    crc_code_list = []
+
+    for i in data['Name']:
+      try:
+        crc_code_list.append(CRC_name_code_dict[i])
+      except:
+        crc_code_list.append("")
+    
+    data['CRC Code'] = crc_code_list
+    
+    data['Super Cheap Auto SKU'] = id_list
+    data['Price'] = price_list
+    data['First Price'] = first_price_list
+    data['Link'] = link_list
+
+    st.dataframe(data)
+    
+    data1 = pd.DataFrame()
+    
+    data1['CRC Code'] = ['3020','3027','3036','3040','3055','3058','3059','3061','3063','3064','3097','3145','3358','3410','5006','5009','5012','5014','5015','5018','5019','5028','5029','5035','5037','5070','5089','5500','1751837','1751839','1751840','1751841','1751842','1751846','1751847','1751848','1752426','1752455','5022','5023','5025','5026','5044','5045','5047','5069','9009','9011','9015','9017','9060','9220','9225','9230','9235','9240','9302','9304','9308','8288','20365','20369','20380','20384','20388','20392','20395','20400','2105','2087','2090','2125','2129','3073','3075','14610','18418','1753336','EVR1','EVR5','4420','4490','4492','5115','7063','7064','7072','7073','7074','7075','8002','8006','8008','8009','8010','8012','8014','8017','8020','8022','8026','8034','8110','8180','8200','8202','8204','8270','8490','8492','8494','8498','1751844','1753108','5040','6028','2015','2016','2018','2053','2071','3013']
+    merged_data = data1.merge(data, on='CRC Code', how='left').fillna("")
+    st.dataframe(merged_data)
 
 
 if st.button("NZ - Scrape PlaceMakers"):
@@ -426,4 +562,7 @@ if st.button("NZ - Scrape The ToolShed"):
 st.markdown('---')
 
 if st.button("AU - Scrape The Total Tools"):
-    on_button_click_TotalTools()
+    on_button_click_AU_TotalTools()
+
+if st.button("AU - Scrape Super Cheap Auto"):
+    on_button_click_NZ_Super()
