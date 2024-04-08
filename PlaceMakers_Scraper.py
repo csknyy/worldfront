d7df15843a9e8a074764a84a543ccd003913e025
csknyy <55134627+csknyy@users.noreply.github.com>
2024-04-08 16:34:11 +1200

    Update PlaceMakers_Scraper.py

--- PlaceMakers_Scraper.py
+++ PlaceMakers_Scraper.py
@@ -845,6 +845,9 @@ if select_text == "Autobarn":
     text_input = st.text_input("Enter Autobarn text here:")
     if len(text_input) > 1:
         text_input = text_input.replace('GDI IVD Intake Valve Cleaner', 'GDI IVD Intake Valve Cleaner - 5319')
+        text_input = text_input.replace('CRC Moto Plastic Polish 200mL- 1752430', 'CRC Moto Plastic Polish 200mL - 1752430')
+
+        CRC Moto Plastic Polish 200mL- 1752430
     
         text_input = text_input.split('CRC')[1:]
         
