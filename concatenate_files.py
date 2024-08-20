8032706866db5d72b82d09effc1a411122ca82f2
csknyy <55134627+csknyy@users.noreply.github.com>
2024-08-20 12:15:40 +1200

    Update concatenate_files.py

--- concatenate_files.py
+++ concatenate_files.py
@@ -3,31 +3,26 @@ import streamlit as st
 
 st.set_page_config(page_title="Concatenate files", layout="wide")
 
-
 def convert_df(df):
-    return df.to_csv(index = False).encode('utf-8')
-
-#file_count = st.text_input('How many files?', '')
-
-#files = [f"file{i}" for i in range(int(file_count))]
+    return df.to_csv(index=False).encode('utf-8')
 
 uploaded_files = st.file_uploader("Choose the CSV files", accept_multiple_files=True)
 file_count = len(uploaded_files)
 
 files = [f"file{i}" for i in range(int(file_count))]
 
-i=0
-for uploaded_file,file in zip(uploaded_files,files):
-    file = pd.read_csv(uploaded_file, index_col=False)
+i = 0
+for uploaded_file, file in zip(uploaded_files, files):
+    file = pd.read_csv(uploaded_file, index_col=False)  # Exclude index when reading the CSV
     files[i] = file
     st.write("Uploaded:", uploaded_file.name)
-    i = i+1
+    i += 1
 
 st.header("Concatenated dataframes")
-result = pd.concat(files)
+result = pd.concat(files, ignore_index=True)  # Concatenate and reset the index
 
 st.dataframe(result)
 
 csv = convert_df(result)
 
-st.download_button(label="Download data as CSV", data=csv,file_name='Concatenated_files.csv', mime='text/csv')
+st.download_button(label="Download data as CSV", data=csv, file_name='Concatenated_files.csv', mime='text/csv')
