f5785862db7cc70b03c5a9689e11f4b7469c0448
csknyy <55134627+csknyy@users.noreply.github.com>
2025-02-18 13:30:01 +1300

    Update concatenate_files.py

--- concatenate_files.py
+++ concatenate_files.py
@@ -6,17 +6,20 @@ st.set_page_config(page_title="Concatenate files", layout="wide")
 def convert_df(df):
     return df.to_csv().encode('utf-8')
 
-uploaded_files = st.file_uploader("Choose the CSV files", accept_multiple_files=True)
+uploaded_files = st.file_uploader("Choose the CSV or Excel files", accept_multiple_files=True)
 file_count = len(uploaded_files)
 
 files = [f"file{i}" for i in range(int(file_count))]
 
-i=0
-for uploaded_file,file in zip(uploaded_files,files):
-    file = pd.read_csv(uploaded_file)
+i = 0
+for uploaded_file, file in zip(uploaded_files, files):
+    if uploaded_file.name.endswith('.csv'):
+        file = pd.read_csv(uploaded_file)
+    elif uploaded_file.name.endswith('.xlsx'):
+        file = pd.read_excel(uploaded_file)
     files[i] = file
     st.write("Uploaded:", uploaded_file.name)
-    i = i+1
+    i += 1
 
 st.header("Concatenated dataframes")
 result = pd.concat(files)
@@ -27,4 +30,4 @@ st.dataframe(result)
 
 csv = convert_df(result)
 
-st.download_button(label="Download data as CSV", data=csv,file_name='Concatenated_files.csv', mime='text/csv')
+st.download_button(label="Download data as CSV", data=csv, file_name='Concatenated_files.csv', mime='text/csv')
